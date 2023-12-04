38772af057a0b707ba387f4997787f4c11b1a120
Johnny Meng <bulldf@Johnny.local>
2023-12-04 11:35:22 -0500

    Ensemble finalized

--- starter_code/part_a/ensemble.py
+++ starter_code/part_a/ensemble.py
@@ -1,54 +1,57 @@
 import numpy as np
+from tqdm import tqdm
 
+import time
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
+from torch.nn import init
 from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
 
 from utils import load_train_csv, load_valid_csv, load_public_test_csv
 from typing import Tuple, List
 
-
 in_shape = 2
-out_shape = 2
+out_shape = 1
+learning_rate = 0.0005
 batch_size = 128
-learning_rate = 0.01
-
 
 class LogisticRegression(nn.Module):
 
-    model: nn.Sequential
+    linear: nn.Linear
 
     def __init__(self) -> None:
         super().__init__()
-        self.model = nn.Sequential(
-            nn.Linear(in_shape, out_shape),
-            nn.Sigmoid()
-        )
+        self.linear = nn.Linear(in_shape, out_shape)
+
+        init.zeros_(self.linear.weight)
 
     def forward(self, input) -> torch.Tensor:
-        output = self.model(input)
+        logits = self.linear(input)
+        output = F.sigmoid(logits)
         return output
     
 
 class NeuralNetwork(nn.Module):
+    
     model: nn.Sequential
 
     def __init__(self) -> None:
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(in_shape, 64),
+            nn.Linear(in_shape, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
             nn.ReLU(),
             nn.Linear(64, 32),
             nn.ReLU(),
-            nn.Linear(32, 16),
-            nn.ReLU(),
-            nn.Linear(16, out_shape),
-            nn.Sigmoid()
+            nn.Linear(32, out_shape)
         )
 
     def forward(self, input) -> torch.Tensor:
-        output = self.model(input)
+        logits = self.model(input)
+        output = F.sigmoid(logits)
         return output
 
 
@@ -59,7 +62,7 @@ class Data(Dataset):
     def __init__(self, data, labels) -> None:
         super().__init__()
         self.data = torch.tensor(data, dtype=torch.float32)
-        self.labels = torch.tensor(labels, dtype=torch.long)
+        self.labels = torch.tensor(labels, dtype=torch.float32)
 
     def __len__(self) -> int:
         return len(self.data)
@@ -73,56 +76,13 @@ def load_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndar
     val = load_valid_csv()
     test = load_public_test_csv()
 
-    train = np.array([train['user_id'], train['question_id'], train['is_correct']]).T
-    val_data = np.array([val['user_id'], val['question_id']]).T
+    train = np.array([train['question_id'], train['user_id'], train['is_correct']]).T
+    val_data = np.array([val['question_id'], val['user_id']]).T
     val_labels = np.array(val['is_correct'])
-    test_data = np.array([test['user_id'], test['question_id']]).T
+    test_data = np.array([test['question_id'], test['user_id']]).T
     test_labels = np.array(test['is_correct'])
 
     return train, val_data, val_labels, test_data, test_labels
-
-
-def training(model: nn.Module, dataloader: DataLoader, epochs: int) -> None:
-
-    model.train()
-
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-    
-    for epoch in range(epochs):
-        count = 0
-        total = 0
-        for data, labels in dataloader:
-            data, labels = data, labels
-            logits = model(data)
-            loss = F.cross_entropy(logits, labels)
-
-            preds = torch.argmax(logits, dim=1)
-            count += torch.sum(preds == labels)
-            total += len(labels)
-
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-        accuracy = count / total
-        print(f'Loss after epoch {epoch + 1} for {model._get_name()}: {loss.item()}')
-        print(f'Training accuracy after epoch {epoch + 1} for {model._get_name()}: {accuracy}')
-
-    
-def evaluate_accuracy(model: nn.Module, dataloader: DataLoader) -> float:
-
-    model.eval()
-    count = 0
-    total = 0
-
-    with torch.no_grad():
-        for data, labels in dataloader:
-            logits = model(data)
-            preds = torch.argmax(logits, dim=1)
-            count += torch.sum(preds == labels)
-            total += len(labels)
-
-    accuracy = count / total
-    return accuracy
 
 
 def bootstrap(train: np.ndarray, num_bootstraps=3) -> List[Tuple[np.ndarray, np.ndarray]]:
@@ -133,10 +93,60 @@ def bootstrap(train: np.ndarray, num_bootstraps=3) -> List[Tuple[np.ndarray, np.
         bootstrap_sample = rng.choice(train, size=n, replace=True)
         train_data = bootstrap_sample[:, :2]
         train_labels = bootstrap_sample[:, 2]
-
         bootstrap_samples.append((train_data, train_labels))
     
     return bootstrap_samples
+
+
+def _training(model: nn.Module, dataloader: DataLoader, epochs: int) -> None:
+
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    
+    losses = []
+    accuracies = []
+
+    for epoch in tqdm(range(epochs), desc="epoch"):
+        model.train()
+        for data, labels in dataloader:
+            probs = model(data).squeeze()
+            loss = F.binary_cross_entropy(probs, labels, reduction='sum')
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        
+        if epoch % 10 == 0:
+            loss, accuracy = evaluate(model, dataloader)
+            losses.append(loss)
+            accuracies.append(accuracy)
+    
+    epoch_indices = [i * 10 for i in range(epochs // 10)]
+    plt.plot(epoch_indices, losses)
+    plt.title('Training Loss')
+    plt.show()
+
+    plt.plot(epoch_indices, accuracies)
+    plt.title('Training Accuracy')
+    plt.show()
+
+    
+def evaluate(model: nn.Module, dataloader: DataLoader) -> Tuple[float, float]:
+
+    model.eval()
+    count = 0
+    total = 0
+    loss = 0
+
+    with torch.no_grad():
+        for data, labels in dataloader:
+            probs = model(data).squeeze()
+            loss += F.binary_cross_entropy(probs, labels, reduction='sum').item()
+            preds = (probs >= 0.5).to(torch.float32)
+            
+            count += torch.sum(preds == labels).item()
+            total += len(labels)
+
+    accuracy = count / total
+    return loss, accuracy
 
 
 def train_eval(model: nn.Module, train_data: np.ndarray,
@@ -145,31 +155,35 @@ def train_eval(model: nn.Module, train_data: np.ndarray,
     training_dataset = Data(train_data, train_labels)
     training_dataloader = DataLoader(training_dataset, batch_size=batch_size)
     
-    training(model, training_dataloader, epochs)
+    _training(model, training_dataloader, epochs)
 
     val_dataset = Data(val_data, val_labels)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
 
-    print(f'Training accuracy for {model._get_name()}: {evaluate_accuracy(model, training_dataloader)}')
-    print(f'Validation accuracy for {model._get_name()}: {evaluate_accuracy(model, val_dataloader)}')
+    training_loss, training_accuracy = evaluate(model, training_dataloader)
+    print(f'Training loss and accuracy for {model._get_name()}: {training_loss}, {training_accuracy}')
+
+    val_loss, val_accuracy = evaluate(model, val_dataloader)
+    print(f'Validation loss and accuracy for {model._get_name()}: {val_loss}, {val_accuracy}')
+
 
 
 def ensemble(models: List[nn.Module], dataloader: DataLoader) -> torch.Tensor:
     all_preds = []
     
-    for model in models:
-        model_preds = torch.tensor([])
-        model.eval()
-        for data, _ in dataloader:
-            with torch.no_grad():
-                logits = model(data)
-                preds = torch.argmax(logits, dim=1)
-                model_preds = torch.cat((model_preds, preds), dim=0)
-
-        all_preds.append(list(model_preds))
+    with torch.no_grad():
+        for model in models:
+            model.eval()
+            model_preds = torch.tensor([])
+            for data, _ in dataloader:
+                probs = model(data).squeeze()
+                preds = (probs > 0.5).to(torch.float32)
+                
+                model_preds = torch.cat((model_preds, preds))
+            all_preds.append(list(model_preds))
 
     sum_preds = torch.sum(torch.tensor(all_preds), dim=0)
-    ensemble_preds = sum_preds > 1.5
+    ensemble_preds = (sum_preds >= 2).to(torch.float32)
 
     return ensemble_preds
         
@@ -182,19 +196,19 @@ def main() -> None:
 
     logistic_model = LogisticRegression()
     logistic_data, logistic_labels = bootstrap_samples[0]
-    train_eval(logistic_model, logistic_data, logistic_labels, val_data, val_labels, 10)
+    train_eval(logistic_model, logistic_data, logistic_labels, val_data,val_labels, 50)
 
     torch.save(logistic_model.state_dict(), 'logistic_model.pth')
 
     neural_network1 = NeuralNetwork()
     neural_network_data1, neural_network_labels1 = bootstrap_samples[1]
-    train_eval(neural_network1, neural_network_data1, neural_network_labels1, val_data, val_labels, 50)
+    train_eval(neural_network1, neural_network_data1, neural_network_labels1, val_data, val_labels, 200)
 
     torch.save(neural_network1.state_dict(), 'neural_network1.pth')
 
     neural_network2 = NeuralNetwork()
     neural_network_data2, neural_network_labels2 = bootstrap_samples[2]
-    train_eval(neural_network2, neural_network_data2, neural_network_labels2, val_data, val_labels, 50)
+    train_eval(neural_network2, neural_network_data2, neural_network_labels2, val_data, val_labels, 200)
 
     torch.save(neural_network2.state_dict(), 'neural_network2.pth')
 
